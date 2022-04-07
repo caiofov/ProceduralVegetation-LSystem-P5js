4d18a12798b9fc8e03a265409d59551836639bb3
caiofov <cfoviana@gmail.com>
2022-04-07 11:38:15 -0300

    fixing saved positions

--- files/PlantGenerator.js
+++ files/PlantGenerator.js
@@ -7,34 +7,34 @@ class PlantGenerator{
         this.expansions = expansions
         this.generated_string = undefined
         this.pace = pace
-        this.points = [initial_point]
+        this.points = []
         
         this.angle_acc = 0 //angle accumulator
         this.default_angle = Math.PI/6 //default angle -> 30 degrees
         
-        this.saved_position = undefined
-        this.saved_angle = undefined
+        this.saved_positions = []
+        this.saved_angles = []
     }
 
     generate(){ //generates the string using the l_system previously defined
         let generated = this.seed
-          
+        
         
         for(let i = 0; i < this.expansions; i++){
           let aux = "" //current parallel statement
           for(let l=0; l<generated.length; l++){
             let s = generated[l]
-            let changed = false
+            let rule_applied = false
             
             for (let st = 0; st < this.l_system_left.length; st++){
                 if (s === this.l_system_left[st]){             
                     aux += this.l_system_right[st]
-                    changed = true
+                    rule_applied = true
                     break
                 }
             }
            
-            if(!changed){
+            if(!rule_applied){
                 aux+=s
             }
           }
@@ -48,20 +48,25 @@ class PlantGenerator{
     draw(){
         stroke(0)
 
-        circle(this.points[0].x, this.points[0].y,4)
+        this.points.forEach(path =>{
+            // circle(path[0].x, path[0].y, 3)
+            for(let l = 0; l < path.length-1; l++){
+                line( path[l].x, path[l].y, path[l+1].x, path[l+1].y)
+                // circle(this.points[l+1].x,this.points[l+1].y,4)
+            }
+        })
         
-        for(let l = 0; l < this.points.length-1; l++){
-            line(this.points[l].x,this.points[l].y,this.points[l+1].x,this.points[l+1].y)
-            // circle(this.points[l+1].x,this.points[l+1].y,4)
-        }
     }
 
     generate_points(){
+        
         let last_point = this.initial_point
         let current_point
+        let pts = [last_point]
         
         for(let l=0; l< this.generated_string.length; l++){
             let current_character = this.generated_string[l]
+            let position_loaded = false
             
       
             switch (current_character){
@@ -81,26 +86,33 @@ class PlantGenerator{
                     break
                 case "]": //load the saved position
                     current_point = this.load_position()
-                    print("posiçao carregada" , current_point)
+                    position_loaded = true
                     break
                     
                 default:
                     break
             }
-            this.points.push(current_point)
-            last_point = current_point
+            
+            if (position_loaded){
+                this.points.push(pts)
+                pts = [current_point]
+                last_point = current_point
+            }
+            else{
+                pts.push(current_point)
+                last_point = current_point
+            }
+            
         }
     }
     
     //moving methods
     move_forward(v){
-        print(v)
         let v2 = v.copy()
         let ang = v2.angleBetween(createVector(1,0)) == 0 ? 0 : this.angle_acc
         
         v2.x = v.x + this.pace*cos(ang)
         v2.y = v.y + this.pace*sin(ang)
-        print("moveu pra frente")
         return v2
     }
     move_left(v) {
@@ -108,7 +120,6 @@ class PlantGenerator{
         this.angle_acc += this.default_angle
         v2.x = v.x + this.pace*cos(this.angle_acc)
         v2.y = v.y + this.pace*sin(this.angle_acc)
-        print("moveu para a esquerda")
         return v2
     }
     move_right(v){
@@ -116,18 +127,16 @@ class PlantGenerator{
         let v2 = this.move_forward(v)
         v2.x = v.x + this.pace*cos(this.angle_acc)
         v2.y = v.y + this.pace*sin(this.angle_acc)
-        print("moveu para a direita")
         return v2
     }
     save_position(current_point){
-        this.saved_position = current_point
-        this.saved_angle = this.angle_acc
-        print("salvou a posiçao")
+        this.saved_positions.push(current_point)
+        this.saved_angles.push(this.angle_acc)
     }
     load_position(){
         
-        this.angle_acc = this.saved_angle
-        return this.saved_position
+        this.angle_acc = this.saved_angles.pop()
+        return this.saved_positions.pop()
        
     }
 }
--- files/sketch.js
+++ files/sketch.js
@@ -6,12 +6,12 @@ function setup() {
   createCanvas(CNV_WIDTH, CNV_HEIGHT);
   initial_point = createVector(INITIAL_X, INITIAL_Y)
   
-  koch = new PlantGenerator(initial_point, PACE,L_SYSTEM_LEFT, L_SYSTEM_RIGHT, SEED,  EXPANSIONS) //creates the a plant
-  koch.generate() //generates the string
-  koch.generate_points() //generates the points
+  plant = new PlantGenerator(initial_point, PACE,L_SYSTEM_LEFT, L_SYSTEM_RIGHT, SEED,  EXPANSIONS) //creates the a plant
+  plant.generate() //generates the string
+  plant.generate_points() //generates the points
 }
 
 function draw() {
   background(240);
-  koch.draw() //draws the plant
+  plant.draw() //draws the plant
 }
